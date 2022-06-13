434b24dae502417210a985ac76ae4b40d40ce92d
unknown <juliana_guonik@hotmail.com>
2022-06-12 21:24:15 -0300

    Adc posts no seeders para verificar rotas

--- backend/src/database/seeders/20220612224248-post.js
+++ backend/src/database/seeders/20220612224248-post.js
@@ -3,11 +3,25 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.bulkInsert('Post', [{
-      user_id: 1,
+      user_id: 3,
       content:'blablabla teste',
       createdAt: new Date(),
       updatedAt: new Date(),
-    }])
+    },
+    {
+      user_id: 4,
+    content:'blablabla teste',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    user_id: 5,
+  content:'blablabla teste',
+  createdAt: new Date(),
+  updatedAt: new Date(),
+},
+
+])
   },
 
   async down (queryInterface, Sequelize) {
